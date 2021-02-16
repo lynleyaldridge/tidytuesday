35f1f55970e38326f270495a2244b38cc782032d
Lynley Aldridge <lynley.aldridge@gmail.com>
2021-02-16 16:35:55 +1100

    edit code and image for compare-sales plot

--- 2020/2020-week40/compare-sales.R
+++ 2020/2020-week40/compare-sales.R
@@ -41,25 +41,7 @@ albums %>%
   tab_spanner(label = "Sales ($ million)", columns = vars(US_sales,
                                                           WW_sales)) %>%
   
-  cols_align(align = "right", columns = c("US_chart", "UK_chart",
-                                          "US_sales", "WW_sales",
-                                          "US_percent")) %>%
-  
-  tab_header(
-    title = md(
-      "**Taylor Swift has higher sales than Beyoncé, but owes a greater proportion of her success to US sales than Beyoncé**"),
-    subtitle = md(
-      "*Peak chart position, sales, and US sales as a percentage of total sales by album*")) %>%
-  
-  tab_source_note(
-    source_note = md(
-      "Source: Billboard via Wikipedia, October 2020")) %>%
-
-  tab_footnote(
-    footnote = md("Excludes 3 albums for which worldwide sales data was not available - Taylor Swift, Folklore, and 4"),
-    locations = cells_title(groups = "subtitle")) %>%
-  
-  # create summary rows for each group 
+ # create summary rows for each group 
   summary_rows(groups = TRUE, 
                columns = vars(US_sales, WW_sales),
                fns = list(TOTAL = "sum"),
@@ -82,9 +64,56 @@ albums %>%
                formatter = fmt_percent, decimals = 1) %>%
   
   # style summary rows and row group titles 
-  tab_style(style = cell_text(weight = "bold"),
+  tab_style(style = cell_text(color = "#795548", weight = "bold"),
             locations = list(cells_summary(groups = TRUE),
-                             cells_row_groups(groups = TRUE))) 
+                             cells_row_groups(groups = TRUE))) %>%
+
+  cols_align(align = "right", columns = c("US_chart", "UK_chart",
+                                          "US_sales", "WW_sales",
+                                          "US_percent")) %>%
+  
+  tab_style(style = cell_text(color = "#795548"),
+            locations = list(
+              cells_column_labels(everything()),
+              cells_stub(rows = TRUE),
+              cells_body()))%>%
+  
+  tab_style(style = cell_text(
+    color = "#795548",
+    weight = "bold"),
+    locations = cells_column_spanners(spanners = vars("Chart position", 
+                                                      "Sales ($ million)")))%>%
+  
+  tab_header(
+    title = md(
+      "**Taylor Swift has higher sales than Beyoncé, but owes a greater proportion of her success to US sales than Beyoncé**"),
+    subtitle = md(
+      "*Peak chart position, sales, and US sales as a percentage of total sales by album*")) %>%
+  
+  tab_source_note(
+    source_note = md("<span style = 'color:#795548'>Source: Billboard via Wikipedia, October 2020</span>")) %>%
+  
+  tab_footnote(
+    footnote = md("<span style = 'color:#795548'>Excludes 3 albums for which worldwide sales data was not available - Taylor Swift, Folklore, and 4</span>"),
+    locations = cells_title(groups = "subtitle")) %>%
+  
+  tab_style(style = cell_text(color = "#795548", size = "large"),
+            locations = cells_title(groups = "title")) %>%
+  
+  tab_style(style = cell_text(color = "#795548", size = "medium"),
+            locations = cells_title(groups = "subtitle")) %>%
+  
+  tab_options(
+    table.border.top.color = '#795548',
+    table.border.bottom.color = '#795548',
+    heading.border.bottom.color = '#795548',
+    stub.border.color = '#795548',
+    column_labels.border.bottom.color = '#795548',
+    row_group.border.top.color = '#795548',
+    row_group.border.bottom.color = '#795548',
+    table_body.hlines.color = '#795548',
+    table_body.border.bottom.color = '#795548',
+    summary_row.border.color = '#795548') 
 
 # %>%
 
